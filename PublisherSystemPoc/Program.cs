64ed2368c4350b25d8700c006aa727e7ec6fcf6f
Vitor Custodio <vitor@MacBook13Home.local>
2019-11-24 12:52:28 -0300

    Ajustado o publisher para transmitir as mgs

--- PublisherSystemPoc/Program.cs
+++ PublisherSystemPoc/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using PublisherSystemPoc.Commons;
+using PublisherSystemPoc.Domain;
 
 namespace PublisherSystemPoc
 {
@@ -6,7 +8,39 @@ namespace PublisherSystemPoc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var publisher = ImplementationsFactory.GetPublisherInstance();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Sistema de Envio de Mensagens");
+                Console.WriteLine("=============================");
+
+                Console.WriteLine("");
+                Console.Write("Informe o Nome de quem envia a Mensagem: ");
+                var _nome = Console.ReadLine();
+                Console.Write("Quantas Mensagens Enviar: ");
+                int _qtd = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enviando ...");
+
+                for (int i = 1; i <= _qtd; i++)
+                {
+                    publisher.SendAsync(new MessageCommand
+                    {
+                        SenderName = _nome,
+                        BodyMessage = $"Esta Mensagem é a numero {i}",
+                    });
+                }
+
+                Console.WriteLine($"Concluido. Foram enviadas {_qtd} mensagens");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.Write("Deseja executar mais uma vez?(S/N)-> ");
+                var response = Console.ReadLine();
+
+                if(response.ToUpper() != "S") break;
+            }
         }
     }
 }
